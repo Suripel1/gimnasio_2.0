b829b42b43f96507918eaab9a2b1246c7f5b9852
Suripel1suripel1 <suripeli04@gmail.com>
2024-06-13 00:05:28 -0600

    app

---
+++ Gimnasio_2.0-main/Gimnasio_2.0-main/Gimnasio2.0-main/FrontEnd/front-gimnasio/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/components/Login.vue'
+import RegisterUserView from '@/components/RegisterUser.vue'
+import DashboardView from '@/components/Dashboard.vue'
+import Persona from '@/components/Persona.vue'
+import Home from '@/components/Home.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUserView
+    }
+    ,
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+      children:[
+        {path:'/home', name: 'home', component:Home},
+        {path:'/personas', name: 'personas', component:Persona}
+      ]
+    }
+  ]
+})
+
+export default router
