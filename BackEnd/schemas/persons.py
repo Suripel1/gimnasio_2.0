d92e8b218683d90ea39c79d9a5ec30dc60843acf
suripel1 <suripeli04@gmail.com>
2024-07-04 11:01:04 -0600

    modificacion del archivo de routs personas

---
+++ BackEnd/schemas/persons.py
@@ -0,0 +1,26 @@
+from typing import List, Union
+from pydantic import BaseModel
+from datetime import datetime
+class PersonBase(BaseModel):
+    Titulo_Cortesia: str
+    Nombre: str
+    Primer_Apellido: str
+    Segundo_Apellido: str
+    Fecha_Nacimiento: datetime
+    Fotografia: str
+    Genero: str
+    Tipo_Sangre: str
+    Estatus: bool
+    Fecha_Registro: datetime
+    Fecha_Actualizacion: datetime
+
+class PersonCreate(PersonBase):
+    pass
+
+class PersonUpdate(PersonBase):
+    pass
+
+class Person(PersonBase):
+    id: int
+    class Config:
+        orm_mode = True
