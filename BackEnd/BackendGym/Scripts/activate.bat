5bca5eba6ed35fbf3bd59c547b9a47e74a8f164e
Suripel1 <suripeli04@gmail.com>
2024-07-17 22:59:14 -0600

    modicacion

--- BackEnd/BackendGym/Scripts/activate.bat
+++ BackEnd/BackendGym/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PC-10\Desktop\gym\gimnasio_2.0\BackEnd\BackendGym
+set VIRTUAL_ENV=C:\Users\HP\Pictures\gimnasio2.0\gimnasio_2.0\BackEnd\BackendGym
 
 if not defined PROMPT set PROMPT=$P$G
 
--- BackEnd/crud/persons.py
+++ BackEnd/crud/persons.py
@@ -1,40 +1,33 @@
 import models.persons
-
 import schemas.persons
-
-
 from sqlalchemy.orm import Session
-
 import models, schemas
-
 
 def get_person(db: Session, id: int):
     return db.query(models.persons.Person).filter(models.persons.Person.ID == id).first()
 
-
-
-
 def get_person_by_nombre(db: Session, person: str):
     return db.query(models.persons.Person).filter(models.persons.Person.Nombre == person).first()
-
-
 
 def get_persons(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.persons.Person).offset(skip).limit(limit).all()
 
-
-
 def create_person(db: Session, person: schemas.persons.PersonCreate):
-    db_person = models.persons.Person(Titulo_Cortesia=person.Titulo_Cortesia, Nombre=person.Nombre, Primer_Apellido=person.Primer_Apellido, Segundo_Apellido=person.Segundo_Apellido, Fecha_Nacimiento=person.Fecha_Nacimiento, Fotografia=person.Fotografia, Genero=person.Genero, Tipo_Sangre=person.Tipo_Sangre, Estatus=person.Estatus, Fecha_Registro=person.Fecha_Registro, Fecha_Actualizacion=person.Fecha_Actualizacion)
+    db_person = models.persons.Person(Titulo_Cortesia=person.Titulo_Cortesia, 
+                                      Nombre=person.Nombre, 
+                                      Primer_Apellido=person.Primer_Apellido, 
+                                      Segundo_Apellido=person.Segundo_Apellido, 
+                                      Fecha_Nacimiento=person.Fecha_Nacimiento, 
+                                      Fotografia=person.Fotografia, 
+                                      Genero=person.Genero, 
+                                      Tipo_Sangre=person.Tipo_Sangre, 
+                                      Estatus=person.Estatus, 
+                                      Fecha_Registro=person.Fecha_Registro, 
+                                      Fecha_Actualizacion=person.Fecha_Actualizacion)
     db.add(db_person)
-
-
     db.commit()
-
     db.refresh(db_person)
     return db_person
-
-
 
 def update_person(db: Session, id: int, person: schemas.persons.PersonUpdate):
     db_person = db.query(models.persons.Person).filter(models.persons.Person.ID == id).first()
@@ -42,9 +35,7 @@ def update_person(db: Session, id: int, person: schemas.persons.PersonUpdate):
         for var, value in vars(person).items():
             setattr(db_person, var, value) if value else None
         db.commit()
-
         db.refresh(db_person)
-        
     return db_person
 
 def delete_person(db: Session, id: int):
