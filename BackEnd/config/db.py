fe13add2b4f7fae035ef041e24a739530fa4a5b8
suripel1 <suripeli04@gmail.com>
2024-07-19 16:36:24 -0600

    config de bd

--- BackEnd/config/db.py
+++ BackEnd/config/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_AqjWcfGnCVnYtG4JTrx@mysql-3bb66c0d-suripeli04-67e5.l.aivencloud.com:22923/defaultdb"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_AqjWcfGnCVnYtG4JTrx@mysql-3bb66c0d-suripeli04-67e5.l.aivencloud.com:22923/defaultdb"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
